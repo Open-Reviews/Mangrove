ef62e4cc4e76e76994b899232e0d410e12c86de4
keorn <git@i.keorn.org>
2019-12-20 17:59:47 +0100

    adjust menus on small screens

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -5,13 +5,32 @@
         <v-img src="mangrove.png" max-width="170" contain />
       </router-link>
       <v-spacer />
-      <v-toolbar-items class="mr-n4">
-        <v-btn to="/build">Build on Mangrove</v-btn>
-        <v-btn to="/contribute">Contribute</v-btn>
-        <v-btn to="/settings">
-          <v-icon>mdi-account-circle</v-icon>
-        </v-btn>
+      <v-toolbar-items class="mr-n4 hidden-sm-and-down">
+        <v-btn
+          v-for="item in menu"
+          :to="item.to"
+          v-html="item.label"
+          :key="item.to"
+        />
+        <v-btn to="/settings"> <v-icon>mdi-account-circle</v-icon></v-btn>
       </v-toolbar-items>
+      <div class="hidden-md-and-up">
+        <v-menu bottom left>
+          <template v-slot:activator="{ on }">
+            <v-btn v-on="on" icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="item in menu" :to="item.to" :key="item.to">
+              <v-list-item-title>{{ item.label }}</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/settings">
+              <v-list-item-title>Settings</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
     </v-app-bar>
     <v-content>
       <v-container style="max-width: 1200px">
@@ -66,6 +85,10 @@ export default {
   data() {
     return {
       title: 'Mangrove',
+      menu: [
+        { to: '/build', label: 'Build on Mangrove' },
+        { to: '/contribute', label: 'Contribute' }
+      ],
       psUrl: 'https://planting.space',
       ccbyUrl: 'https://creativecommons.org/licenses/by/4.0',
       internals: [
