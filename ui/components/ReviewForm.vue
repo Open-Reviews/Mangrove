beb69d2d48daaa1c8a789e5e47a13c727f60d5d5
keorn <git@i.keorn.org>
2020-02-06 13:59:10 +0100

    show warning only on ff

--- ui/components/ReviewForm.vue
+++ ui/components/ReviewForm.vue
@@ -4,8 +4,13 @@
       <v-card>
         <v-card-title v-html="subjectLine" class="justify-center" />
         <v-divider />
-        <v-alert type="warning" elevation="8" border="left">
-          Image upload and review submission may not work on Firefox Mobile.
+        <v-alert
+          v-if="isMobileFirefox"
+          type="warning"
+          elevation="8"
+          border="left"
+        >
+          Image upload may not work on Firefox Mobile.
         </v-alert>
         <v-rating v-model="rating" hover class="my-1 text-center" large />
         <v-card-text>
