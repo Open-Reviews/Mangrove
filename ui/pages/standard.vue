3adc7b2dbe275b2dd84bedd676cc7febfc9e57b6
keorn <git@i.keorn.org>
2020-02-12 15:03:32 +0100

    add test to js lib

---
+++ ui/pages/standard.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container v-html="html" style="max-width: 1200px" />
+</template>
+
+<script>
+import { html } from '../../Mangrove_Review_Standard.md'
+
+export default {
+  data() {
+    return { html }
+  }
+}
+</script>
