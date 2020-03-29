a5ce60397433a4bc72fe97f5da26fa223fea7bc3
keorn <git@i.keorn.org>
2020-03-29 16:12:36 +0200

    actually display error

--- ui/layouts/error.vue
+++ ui/layouts/error.vue
@@ -1,10 +1,7 @@
 <template>
   <v-app>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
+    <h1>
+      {{ head }}
     </h1>
     <NuxtLink to="/">
       Home page
