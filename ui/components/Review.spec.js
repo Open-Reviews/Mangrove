9530c1da448cb02938084f4b95f75d1b738abb35
roschaefer <git@roschaefer.de>
2020-09-18 23:56:27 +0200

    test: First meaningful component test

--- ui/components/Review.spec.js
+++ ui/components/Review.spec.js
@@ -1,11 +1,37 @@
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import lineClamp from 'vue-line-clamp'
 import Review from './Review.vue'
 
+Vue.use(Vuetify)
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(lineClamp)
+
 describe('Review', () => {
+  let getters
+  let store
+  let vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+    getters = {
+      state: () => ({
+        publicKey: 'abcdefg'
+      })
+    }
+
+    store = new Vuex.Store({
+      getters
+    })
+  })
+
   it('renders', () => {
-    const wrapper = shallowMount(Review)
-    expect(wrapper.contains('div')).toBe(true)
+    const wrapper = shallowMount(Review, { store, localVue, vuetify })
+    expect(wrapper.find('div').exists()).toBe(true)
   })
 })
-
-
